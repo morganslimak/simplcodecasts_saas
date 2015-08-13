6233768e112ecca232a21f2d49435181b5629535
Morgan <morganslimak@gmail.com>
2015-08-13 21:16:01 +0000

    fixed typo

--- db/migrate/20150813192404_create_profiles.rb
+++ db/migrate/20150813192404_create_profiles.rb
@@ -1,6 +1,6 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
-    create_table :profiles do |t
+    create_table :profiles do |t|
       t.integer :user_id
       t.string :first_name
       t.string :last_name
